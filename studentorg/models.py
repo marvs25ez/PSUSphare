385fc64ac359de195c4a6812e4e98f92c70302be
covybryan <covybryan717@gmail.com>
2025-04-04 16:21:05 +0800

    models class configuration

--- studentorg/models.py
+++ studentorg/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model) :
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at =models.DateTimeField(auto_now_add=True)
+
+    class Meta :
+        abstract = True
+
+class college(BaseModel):
+    college_name = models.CharField(max_length=150)
+'''
+    def__str__(self):
+        return self.college_name
+'''
+class program(BaseModel):
+    prog_name =  models.CharField(max_length=150)
+    college =  models.ForeignKey(College, on_delete=models.CASCADE)
+
+
+
