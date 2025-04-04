7e5014cedb90e83ca2876514a55f62f6eb44473f
covybryan <covybryan717@gmail.com>
2025-04-04 20:43:11 +0800

    models and admin crated

--- studentorg/admin.py
+++ studentorg/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import College, program, Organization, Student, OrgMember
+admin.site.register(College)
+admin.site.register(program)
+admin.site.register(Organization)
+admin.site.register(Student)
+admin.site.register(OrgMember)
+
+
--- studentorg/models.py
+++ studentorg/models.py
@@ -8,15 +8,39 @@ class BaseModel(models.Model) :
     class Meta :
         abstract = True
 
-class college(BaseModel):
+class College(BaseModel):
     college_name = models.CharField(max_length=150)
-'''
-    def__str__(self):
+
+    def __str__(self):
         return self.college_name
-'''
+
 class program(BaseModel):
     prog_name =  models.CharField(max_length=150)
     college =  models.ForeignKey(College, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.prog_name
+    
+class Organization(BaseModel):
+    name= models.CharField(max_length=250)
+    college = models.ForeignKey(
+        College, null=True, blank=True, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+class Student(BaseModel):
+    student_id = models.CharField(max_length=15)
+    lastname = models.CharField(max_length=25)
+    firstname = models.CharField(max_length=25)
+    middlename = models.CharField(max_length=25, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.lastname},{self.firstname}"
+    
+class OrgMember(BaseModel):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    date_joined = models.DateField()
 
 
